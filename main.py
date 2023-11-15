21048a0a12b48943ba59e5291354ed093c84a429
dausadu04 <dausadu15@gmail.com>
2023-11-15 18:40:03 +0600

    main

--- main.py
+++ main.py
@@ -1,173 +1,66 @@
-def task1():
-    number = [4, 8, 15, 16, 23, 42]
-
-    for i in number:
-        print(i, end=" ")
+import random
 
 
-# task1()
+class Man:
 
-def task2():
-    number = [4, 8, 15, 16, 23, 42]
+    def __init__(self, name):
+        self.name = name
+        self.food = 10
+        self.fullness = 10
+        self.money = 50
+        self.perception = 'good'
 
-    for i in number:
-        print(i, " ")
-
-
-# task2()
-
-def task3():
-    default_numbers = {9, 10}
-
-    try:
-        user_input = int(input("Enter your number: "))
-        output_numbers = {user_input} | default_numbers
-
-        for number in output_numbers:
-            print(number)
-
-    except ValueError:
-        for number in default_numbers:
-            print(number)
+    def eat(self):
+        self.fullness += 10
+        self.food -= 10
+        self.perception = 'satisfied'
+        print(f"{self.name} ate, now {self.perception}")
+        print(f"Money - {self.money}, Food - {self.food}, Self-perception - {self.perception}, Fullness - {self.fullness}")
 
 
-# task3()
+    def work(self):
+        self.money += 50
+        self.fullness -= 10
+        self.perception = 'tired'
+        print(f"{self.name} worked, now self-perception is {self.perception}")
+        print(f"Money - {self.money}, Food - {self.food}, Self-perception - {self.perception}, Fullness - {self.fullness}")
 
-def task4():
-    try:
-        first_number = int(input("first number: "))
-        second_number = int(input("second number: "))
-        third_number = int(input("third number: "))
+    def play_the_game(self):
+        self.perception = 'great'
+        self.fullness -= 10
+        print(f"{self.name} played, now self-perception is {self.perception}")
+        print(f"Money - {self.money}, Food - {self.food}, Self-perception - {self.perception}, Fullness - {self.fullness}")
 
-        numbers = {first_number, second_number, third_number}
+    def buying_the_food(self):
+        self.money -= 25
+        self.food += 25
+        self.perception = 'good'
+        print(f"{self.name} played, now self-perception is {self.perception}")
+        print(f"Money - {self.money}, Food - {self.food}, Self-perception - {self.perception}, Fullness - {self.fullness}")
 
-        total_numbers = first_number + second_number + third_number
+    def act(self):
+        action = random.randint(1, 5)
 
-        for i in numbers:
-            print(i)
-
-        print(total_numbers)
-    except TypeError:
-        print("Please, write down only numbers, not other")
-
-
-# task4()
-
-
-def task5():
-    try:
-        user_input = int(input(""))
-
-        total = (user_input ** 3)
-
-        total_surface_area = 6 * (user_input ** 2)
-
-        print("Volume = ", total)
-        print("Total surface area", total_surface_area)
-
-    except TypeError:
-        print("Please, write down only numbers, not other")
-
-
-# task5()
-
-
-def task2_1():
-    try:
-        student = int(input("student: "))
-        mandarin = int(input("mandarin: "))
-
-        basket = mandarin % student
-
-        total = mandarin // student
-
-        print(total)
-        print(basket)
-
-    except TypeError:
-        print("Please, write down only numbers, not other")
-
-
-# task2_1()
-
-def task2_2():
-    try:
-        user_input = int(input("Your number: "))
-        number = 999
-
-        if user_input > number:
-            str_user_input = str(user_input)
-            print("The digit in the thousands position is ", str_user_input[0])
-            print("The number in the hundreds position is ", str_user_input[1])
-            print("The digit in the tens position is ", str_user_input[2])
-            print("The digit in the position of units  ", str_user_input[3])
+        if action == 1:
+            self.eat()
+        elif action == 2:
+            if self.fullness < 10:
+                if self.money >= 10:
+                    self.buying_the_food()
+            self.work()
         else:
-            print("Your digit is not a four-digit number")
-
-    except TypeError:
-        print("Please, write down only numbers, not other")
-
-
-# task2_2()
-
-
-def task2_3():
-    try:
-        people = int(input("People: "))
-
-        float_num = people % 2
-
-        if float_num > 0:
-            people += 1
-            print(people)
-    except TypeError:
-        print("Please, write down only numbers, not other")
+            if self.fullness < 10:
+                if self.money >= 10:
+                    self.buying_the_food()
+                else:
+                    if self.food == 0:
+                        print("{} died!".format(self.name))
+                    self.eat()
+            self.play_the_game()
 
 
-# task2_3()
-
-
-def task2_4():
-    try:
-        user_input = int(input("Your input: "))
-
-        if user_input == 0:
-            print("Your number is zero")
-        else:
-            print("The result of << is", user_input + 1)
-
-    except TypeError:
-        print("Please, write down only numbers, not other")
-
-
-# task2_4()
-
-def task2_5():
-    try:
-        num1 = float(input("Please enter the first number: "))
-        num2 = float(input("Please enter the second number: "))
-        operation = input("Please choose the operation (+, -, *, /): ")
-
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            if num2 == 0:
-                print("Error: Division by zero is not allowed.")
-            else:
-                result = num1 / num2
-        else:
-            print("Error: Invalid operation")
-
-        if operation in ('+', '-', '*', '/') and num2 != 0:
-            print(f"{num1} {operation} {num2} = {result:.3f}")
-
-    except ValueError:
-        print("Error: Invalid input. Please enter valid numbers.")
-    except Exception as dauletsuper:
-        print(f"An error occurred: {dauletsuper}")
-
-# task2_5()
+Daulet = Man('Daulet')
+for day in range(1, 31):
+    print("==================Day {}==================".format(day))
+    Daulet.act()
+    print()
